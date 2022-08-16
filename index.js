0feefe6442b48e00db989a21438649dbaa75717b
Twentyquid <negativeradiations@gmail.com>
2022-08-16 20:35:45 +0530

    modified the maximum limit of uploaded json

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.json({
+    limit: "3mb",
+  })
+);
+app.use(express.urlencoded({ extended: true, limit: "3mb" }));
 
 app.use("/feed", feedRoute);
 
